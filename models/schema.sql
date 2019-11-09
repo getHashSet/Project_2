71b964fce60e184e3749e81b6bf961755a98b359
Matthew <matthew_carpenter@icloud.com>
2019-11-09 11:50:16 -0700

    updated table names

--- models/schema.sql
+++ models/schema.sql
@@ -1,10 +1,7 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS nextStepResume_db;
+CREATE DATABASE nextStepResume_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
-
-CREATE TABLE Persons (
+CREATE TABLE user_table (
     id int NOT NULL AUTO_INCREMENT,
     user_password varchar(32),
     user_pin int(8),
