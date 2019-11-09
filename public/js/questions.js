70c6a3553fcb8b39bec00e5f6afaaea6b173da9d
Matthew <matthew_carpenter@icloud.com>
2019-11-09 14:25:16 -0700

    updated api data phase 1.

---
+++ public/js/questions.js
@@ -0,0 +1,23 @@
+/* 
+   this is for loading the 20 questions to build the resume.
+*/
+
+console.log(`--------------------------------------------------------------------------------`);
+console.log(`questions.js loaded.`);
+console.log(`--------------------------------------------------------------------------------`);
+
+$( document ).ready(function() {
+console.log(`app.js ready!`);
+
+/* ----- Code Start ----- */
+
+
+
+
+
+
+/* ------ Code End ------ */
+
+});
+console.log(`loading...`);
+
